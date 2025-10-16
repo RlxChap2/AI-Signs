a1b84eeb197b0787a13061f38b4aa07e1df2abb2
RlxChap2 <mmah02778@gmail.com>
2025-10-16 20:16:36 +0300

    v2 Fixed Flask PORT

--- server.py
+++ server.py
@@ -125,5 +125,5 @@ def analyze():
 # تشغيل السيرفر
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
